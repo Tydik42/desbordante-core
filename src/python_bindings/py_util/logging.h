dcebf715fd77e653291d7134a091e2d00ae2f644
Tydik42 <tgaraev653@gmail.com>
2025-04-08 18:53:12 +0000

    easyogging replaced by sdlog

---
+++ src/python_bindings/py_util/logging.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+#include <pybind11/pybind11.h>
+
+namespace python_bindings {
+void BindLogging(pybind11::module_& main_module);
+}  // namespace python_bindings
